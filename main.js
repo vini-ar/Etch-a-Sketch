98d6a314a464e2d70c473dd4a32c803b10521311
Marcus Vinicyus <125088362+vini-ar@users.noreply.github.com>
2024-08-14 10:14:00 -0300

    Create mousehover effect. When the user hover a box that boxes will turn pink but when the mouse is out, the box will reset

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ function spanDiv() {
         let square = document.createElement("div")
         square.setAttribute("class","square");
         square.textContent = i;
+
+        square.addEventListener("mouseover", function()  { this.classList.add("square-hover") })
+        square.addEventListener("mouseout", function()  {this.classList.remove("square-hover")})
+
         container.appendChild(square)
     }
+}
+
+function changeDivColor() {
+
 }
 
 spanDiv()
