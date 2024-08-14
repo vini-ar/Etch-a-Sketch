19edc5e4cbe47f754891733b8eddfcefea875eea
Marcus Vinicyus <125088362+vini-ar@users.noreply.github.com>
2024-08-14 09:53:42 -0300

    Create function that spans 16 divs

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function spanDiv() {
     for (let i = 0; i < 16; i++) {
         let square = document.createElement("div")
         square.setAttribute("class","square");
-        square.textContent = "Hello World"
+        square.textContent = i;
         container.appendChild(square)
     }
 }
